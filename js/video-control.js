a879dbe836dab2ce5417b35c9ba558e853b09a2a
bieudo1 <103914756+bieudo1@users.noreply.github.com>
2023-10-01 19:34:26 +0700

    update image

--- js/video-control.js
+++ js/video-control.js
@@ -13,18 +13,11 @@ document.addEventListener("keydown", function (event) {
     closeModalFunction();
   }
 });
-
-function video1() {
-  openModalFunction();
-  document.getElementById(
-    "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1lL61bn40H0be-3HLNNIKBKiEGqBSHO3i/preview" width="640" height="480" allow="autoplay"></iframe>`;
-}
 function video2() {
   openModalFunction();
   document.getElementById(
     "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1tLW_Y7O1JlhlPtuMlXZEiedvM9CTMW1Q/preview" width="640" height="480" allow="autoplay"></iframe>`;
+  ).innerHTML = `<iframe src="https://drive.google.com/file/d/1ilH5Uk0BkYBW-eLJXjA6m1c51sp9OvHN/preview" width="640" height="480" allow="autoplay"></iframe>`;
 }
 function video3() {
   openModalFunction();
@@ -38,12 +31,6 @@ function video4() {
     "feedback"
   ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/11uG4UiV70FiQiM7kmBlRyIaopBJk-Z5j/preview" width="640" height="480" allow="autoplay"></iframe>`;
 }
-function video5() {
-  openModalFunction();
-  document.getElementById(
-    "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1a6PXCybWNqj3RaF6rxBoTSkL_y0EkKam/preview" width="640" height="480" allow="autoplay"></iframe>`;
-}
 function video6() {
   openModalFunction();
   document.getElementById(
@@ -54,29 +41,11 @@ function video7() {
   openModalFunction();
   document.getElementById(
     "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1ylBQjKyp0BH7XXhe1iUIXkH7QxwZd7EV/preview" width="640" height="480" allow="autoplay"></iframe>`;
-}
-function video8() {
-  openModalFunction();
-  document.getElementById(
-    "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/187Zza40cNGGOQ9requ34MoS_nmpOsy3l/preview" width="640" height="480" allow="autoplay"></iframe>`;
+  ).innerHTML = `<iframe src="https://drive.google.com/file/d/1avexx3rFYpRXLe3hncid8Bbu6LBO4_lK/preview" width="640" height="480" allow="autoplay"></iframe>`;
 }
 function video9() {
   openModalFunction();
   document.getElementById(
     "feedback"
   ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1q7uU7lGmhiHswLBjxZs8AT1FmlKpnW6S/preview" width="640" height="480" allow="autoplay"></iframe>`;
-}
-function video10() {
-  openModalFunction();
-  document.getElementById(
-    "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1-uSl8SmAVeKC5kwzLb3Sn2oRdsX6Byyr/preview" width="640" height="480" allow="autoplay"></iframe>`;
-}
-function video11() {
-  openModalFunction();
-  document.getElementById(
-    "feedback"
-  ).innerHTML = `<iframe class="video" src="https://drive.google.com/file/d/1cLyA8TEeuX8FxLBN_dbgEciYF9ewnHtX/preview" width="640" height="480" allow="autoplay"></iframe>`;
 }
